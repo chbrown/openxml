5d705cef25450dddbbead8b4cb6fe8e59649a87e
Christopher Brown <io@henrian.com>
2013-01-03 20:53:26 -0600

    Trying to simplify/fix the setuptools external files inclusion.

--- setup.py
+++ setup.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
+from setuptools import setup
+import os
+# here = os.path.abspath(os.path.dirname(__file__))
 
-from distutils.core import setup
-from glob import glob
 
-# Make data go into site-packages (http://tinyurl.com/site-pkg)
-from distutils.command.install import INSTALL_SCHEMES
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
+def recurse(root, folders):
+    cwd = os.getcwd()
+    os.chdir(root)
+    exclude = set(['.DS_Store'])
+    for folder in folders:
+        for dirpath, _, filenames in os.walk(folder):
+            if len(set(filenames) - exclude) > 0:
+                yield os.path.join(dirpath, '*')
+    os.chdir(cwd)
 
-setup(name='python-openxml',
-      version='0.1',
-      requires=['lxml'],
-      description='Create .pptx and .docx files from Python',
-      author='Tom Scrace',
-      author_email='tom.scrace@timetric.com',
-      url='http://github.com/timetric/python-openxml',
-      packages=['openxml'],
-      data_files=[
-          ('openxml/docx_template/_rels', glob('docx_template/_rels/.*')),
-          ('openxml/docx_template/docProps', glob('docx_template/docProps/*.*')),
-          ('openxml/docx_template/word', glob('docx_template/word/*.xml')),
-          ('openxml/docx_template/word/theme', glob('docx_template/word/theme/*.*')),
-          ('openxml/pptx_template/_rels', glob('pptx_template/_rels/.*')),
-          ('openxml/pptx_template/ppt', glob('pptx_template/ppt/*.xml')),
-          ],
-      )
+package_data_folders = list(recurse('openxml', ['docx_template', 'pptx_template']))
+
+setup(name='openxml',
+    version='0.1.1',
+    description='Create .pptx and .docx files from Python',
+    author='Tom Scrace, Christopher Brown',
+    author_email='tom.scrace@timetric.com, audiere@gmail.com',
+    license='MIT',
+    url='http://github.com/chbrown/openxml',
+    packages=['openxml'],
+    install_requires=['lxml'],
+    include_package_data=True,
+    zip_safe=False
+    # package_data={'openxml': package_data_folders}
+)
